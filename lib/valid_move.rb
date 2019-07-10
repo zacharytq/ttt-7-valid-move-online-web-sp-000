80e028321cc444577ff62e49d14f108521a96a2d
chic-page-6502 <zacharytq@gmail.com>
2019-07-10 02:13:16 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,4 +7,13 @@ def position_taken?(board, index)
   end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(board, index)
+  if index > 8
+    false
+  elsif !position_taken?(board, index)
+    false
+  else
+    true
+  end
+
+end
